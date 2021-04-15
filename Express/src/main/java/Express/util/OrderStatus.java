218e19cc2a88a10de4f3c675726174a0efa1e456
Snow02203835 <snow02203835@163.com>
2021-04-15 20:20:10 +0800

    修改 已送达的code改为2 新增 已取消状态

--- Express/src/main/java/Express/util/OrderStatus.java
+++ Express/src/main/java/Express/util/OrderStatus.java
@@ -4,8 +4,8 @@ public enum OrderStatus {
 
     PICKED((byte)0, "已接单"),
     COLLECTED((byte)1, "已取件"),
-
-    SENT((byte)8, "已送达"),
+    SENT((byte)2, "已送达"),
+    CANCEL((byte)3, "已取消"),
     SATISFY((byte)9, "已完成");
 
     private Byte code;
