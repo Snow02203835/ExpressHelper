3f174eb091d23e2565bd16333a088c71c95070d7
Snow02203835 <snow02203835@163.com>
2021-04-15 19:40:28 +0800

    新增 需求状态为已取消的数据

--- Express/src/main/resources/data.sql
+++ Express/src/main/resources/data.sql
@@ -12,5 +12,7 @@ INSERT INTO `demand` VALUES (5,0,'string','string','string','string',2,1,0,NULL,
 INSERT INTO `demand` VALUES (6,0,'string','string','string','string',2,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
 INSERT INTO `demand` VALUES (7,0,'string','string','string','string',3,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
 INSERT INTO `demand` VALUES (8,0,'string','string','string','string',3,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (9,0,'string','string','string','string',4,1,0,NULL,0,'2021-04-15 18:00:00',NULL);
+INSERT INTO `demand` VALUES (10,0,'string','string','string','string',4,1,0,NULL,1,'2021-04-15 18:00:00',NULL);
 /*!40000 ALTER TABLE `demand` ENABLE KEYS */;
 UNLOCK TABLES;
