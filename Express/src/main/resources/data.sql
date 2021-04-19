b26c344201cf97b90b7425bec043c40e3a678368
Snow02203835 <snow02203835@163.com>
2021-04-19 11:35:51 +0800

    新增 demand表预期送达时间数据

--- Express/src/main/resources/data.sql
+++ Express/src/main/resources/data.sql
@@ -4,15 +4,15 @@
 
 LOCK TABLES `demand` WRITE;
 /*!40000 ALTER TABLE `demand` DISABLE KEYS */;
-INSERT INTO `demand` VALUES (1,0,'string','string','string','string',0,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (2,0,'string','string','string','string',0,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (3,0,'string','string','string','string',1,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (4,0,'string','string','string','string',1,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (5,0,'string','string','string','string',2,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (6,0,'string','string','string','string',2,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (7,0,'string','string','string','string',3,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (8,0,'string','string','string','string',3,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
-INSERT INTO `demand` VALUES (9,0,'string','string','string','string',4,1,0,NULL,0,'2021-04-15 18:00:00',NULL);
-INSERT INTO `demand` VALUES (10,0,'string','string','string','string',4,1,0,NULL,1,'2021-04-15 18:00:00',NULL);
+INSERT INTO `demand` VALUES (1,0,'string','string','string','string','string',0,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (2,0,'string','string','string','string','string',0,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (3,0,'string','string','string','string','string',1,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (4,0,'string','string','string','string','string',1,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (5,0,'string','string','string','string','string',2,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (6,0,'string','string','string','string','string',2,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (7,0,'string','string','string','string','string',3,1,0,NULL,0,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (8,0,'string','string','string','string','string',3,1,0,NULL,1,'2021-04-15 17:13:50',NULL);
+INSERT INTO `demand` VALUES (9,0,'string','string','string','string','string',4,1,0,NULL,0,'2021-04-15 18:00:00',NULL);
+INSERT INTO `demand` VALUES (10,0,'string','string','string','string','string',4,1,0,NULL,1,'2021-04-15 18:00:00',NULL);
 /*!40000 ALTER TABLE `demand` ENABLE KEYS */;
 UNLOCK TABLES;
