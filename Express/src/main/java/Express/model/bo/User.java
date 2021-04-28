2941b4d9430b4cec0fe9adbaf93bc00cd0c9f41e
Snow02203835 <snow02203835@163.com>
2021-04-28 09:01:49 +0800

    新增 姓名、学号以及学生认证属性并新增相应赋值

--- Express/src/main/java/Express/model/bo/User.java
+++ Express/src/main/java/Express/model/bo/User.java
@@ -5,6 +5,7 @@ import Core.util.AES;
 import Core.util.Common;
 import Core.util.SHA256;
 import Express.model.po.UserPo;
+import Express.model.vo.UserInfoVo;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -12,16 +13,20 @@ import java.time.LocalDateTime;
 
 /**
  * @author snow create 2021/04/27 19:51
+ *            modified 2021/04/28 09:00
  */
 @Data
 public class User implements VoObject, Serializable {
 
     private final static String userKey = "express-help-2021-04-27-19-55";
     private Long id;
+    private String name;
     private String openId;
     private Integer credit;
     private String mobile;
     private String address;
+    private String studentNumber;
+    private Byte studentVerify;
     private String signature;
     private LocalDateTime gmtCreate;
     private LocalDateTime gmtModified;
@@ -29,15 +34,19 @@ public class User implements VoObject, Serializable {
     public User(String openId){
         this.credit = 100;
         this.openId = openId;
+        this.studentVerify = (byte)0;
         this.signature = createSignature();
     }
 
     public User(UserPo userPo){
         this.id = userPo.getId();
+        this.name = userPo.getRealName();
         this.openId = userPo.getOpenId();
         this.credit = userPo.getCredit();
         this.mobile = userPo.getMobile();
         this.address = userPo.getAddress();
+        this.studentNumber = userPo.getStudentNumber();
+        this.studentVerify = userPo.getStudentVerify();
         this.signature = userPo.getSignature();
         this.gmtCreate = userPo.getGmtCreate();
         this.gmtModified = userPo.getGmtModified();
@@ -46,25 +55,37 @@ public class User implements VoObject, Serializable {
     public UserPo createPo(){
         UserPo userPo = new UserPo();
         userPo.setId(this.id);
+        userPo.setRealName(this.name);
         userPo.setOpenId(this.openId);
         userPo.setCredit(this.credit);
         userPo.setMobile(this.mobile);
         userPo.setAddress(this.address);
+        userPo.setStudentNumber(this.studentNumber);
+        userPo.setStudentVerify(this.studentVerify);
         userPo.setSignature(this.signature);
         userPo.setGmtCreate(this.gmtCreate);
         userPo.setGmtModified(this.gmtModified);
         return userPo;
     }
 
-    public Boolean updateInfoSelective(String address, String mobile){
-        if(address == null && mobile == null){
+    public Boolean updateInfoSelective(UserInfoVo userInfoVo){
+        if(userInfoVo.getAddress() == null &&
+                userInfoVo.getMobile() == null &&
+                userInfoVo.getName() == null &&
+                userInfoVo.getStudentNumber() == null){
             return false;
         }
-        if(mobile != null){
-            setMobile(mobile);
+        if(userInfoVo.getName() != null){
+            this.name = userInfoVo.getName();
         }
-        if(address != null){
-            setAddress(address);
+        if(userInfoVo.getMobile() != null){
+            setMobile(userInfoVo.getMobile());
+        }
+        if(userInfoVo.getAddress() != null){
+            setAddress(userInfoVo.getAddress());
+        }
+        if(userInfoVo.getStudentNumber() != null){
+            this.studentNumber = userInfoVo.getStudentNumber();
         }
         this.signature = createSignature();
         return true;
@@ -89,10 +110,12 @@ public class User implements VoObject, Serializable {
     /**
      * 生成签名
      * @author snow create 2021/01/19 00:25
+     *            modified 2021/04/28 08:48
      * @return
      */
     public String createSignature(){
-        StringBuilder signature = Common.concatString("-", this.openId, this.credit.toString(), this.mobile, this.address);
+        StringBuilder signature = Common.concatString("-", this.openId, this.name,
+                this.credit.toString(), this.mobile, this.address, this.studentNumber, this.studentVerify.toString());
         return SHA256.getSHA256(signature.toString());
     }
 
