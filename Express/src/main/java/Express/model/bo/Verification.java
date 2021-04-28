c50daf6d958741395fd54109aecad506912c7a9b
Snow02203835 <snow02203835@163.com>
2021-04-29 01:23:00 +0800

    新增 Verification.java

---
+++ Express/src/main/java/Express/model/bo/Verification.java
@@ -0,0 +1,63 @@
+package Express.model.bo;
+
+import Core.model.VoObject;
+import Express.model.po.VerificationPo;
+import Express.model.vo.VerificationVo;
+import Express.util.VerificationStatus;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author snow create 2021/04/29 00:58
+ */
+@Data
+public class Verification implements VoObject, Serializable {
+    private Long id;
+    private Long userId;
+    private Byte status;
+    private String coverImg;
+    private String contentImg;
+    private LocalDateTime gmtCreate;
+    private LocalDateTime gmtModified;
+
+    public Verification(Long userId, VerificationVo vo){
+        this.status = VerificationStatus.UNHANDLED.getCode();
+        this.userId = userId;
+        this.coverImg = vo.getCoverImg();
+        this.contentImg = vo.getContentImg();
+    }
+
+    public Verification(VerificationPo po){
+        this.id = po.getId();
+        this.userId = po.getUserId();
+        this.status = po.getStatus();
+        this.coverImg = po.getCoverImg();
+        this.contentImg = po.getContentImg();
+        this.gmtCreate = po.getGmtCreate();
+        this.gmtModified = po.getGmtModified();
+    }
+
+    public VerificationPo createPo(){
+        VerificationPo po = new VerificationPo();
+        po.setId(this.id);
+        po.setUserId(this.userId);
+        po.setStatus(this.status);
+        po.setCoverImg(this.coverImg);
+        po.setContentImg(this.contentImg);
+        po.setGmtCreate(this.gmtCreate);
+        po.setGmtModified(this.gmtModified);
+        return po;
+    }
+
+    @Override
+    public Object createVo() {
+        return this;
+    }
+
+    @Override
+    public Object createSimpleVo() {
+        return null;
+    }
+}
