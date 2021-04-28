85616edc42002d961ce19275fd07e5debd1600ce
Snow02203835 <snow02203835@163.com>
2021-04-28 09:21:01 +0800

    新增 接单者电话以及各种订单状态变化时间属性

--- Express/src/main/java/Express/model/bo/Order.java
+++ Express/src/main/java/Express/model/bo/Order.java
@@ -17,10 +17,16 @@ public class Order implements VoObject, Serializable {
     private Long id;
     private Long demandId;
     private Long receiverId;
+    private String receiverMobile;
     private Byte status;
     private Byte deleted;
     private String urlCheck;
     private String urlSent;
+    private LocalDateTime pickUpTime;
+    private LocalDateTime collectTime;
+    private LocalDateTime sentTime;
+    private LocalDateTime satisfyTime;
+    private LocalDateTime cancelTime;
     private LocalDateTime gmtCreate;
     private LocalDateTime gmtModified;
 
@@ -28,16 +34,23 @@ public class Order implements VoObject, Serializable {
         this.id = orderPo.getId();
         this.demandId = orderPo.getDemandId();
         this.receiverId = orderPo.getReceiverId();
+        this.receiverMobile = orderPo.getReceiverMobile();
         this.status = orderPo.getStatus();
         this.deleted = orderPo.getDeleted();
         this.urlCheck = orderPo.getUrlCheck();
         this.urlSent = orderPo.getUrlSent();
+        this.pickUpTime = orderPo.getPickUpTime();
+        this.collectTime = orderPo.getCollectTime();
+        this.sentTime = orderPo.getSentTime();
+        this.satisfyTime = orderPo.getSatisfyTime();
+        this.cancelTime = orderPo.getCancelTime();
         this.gmtCreate = orderPo.getGmtCreate();
         this.gmtModified = orderPo.getGmtModified();
     }
 
-    public Order(Long receiverId, Long demandId){
+    public Order(Long receiverId, String mobile, Long demandId){
         this.demandId = demandId;
+        this.receiverMobile = mobile;
         this.receiverId = receiverId;
         this.status = OrderStatus.PICKED.getCode();
         this.deleted = (byte)0;
@@ -48,10 +61,16 @@ public class Order implements VoObject, Serializable {
         orderPo.setId(this.id);
         orderPo.setDemandId(this.demandId);
         orderPo.setReceiverId(this.receiverId);
+        orderPo.setReceiverMobile(this.receiverMobile);
         orderPo.setStatus(this.status);
         orderPo.setDeleted(this.deleted);
         orderPo.setUrlCheck(this.urlCheck);
         orderPo.setUrlSent(this.urlSent);
+        orderPo.setPickUpTime(this.pickUpTime);
+        orderPo.setCollectTime(this.collectTime);
+        orderPo.setSentTime(this.sentTime);
+        orderPo.setSatisfyTime(this.satisfyTime);
+        orderPo.setCancelTime(this.cancelTime);
         orderPo.setGmtCreate(this.gmtCreate);
         orderPo.setGmtModified(this.gmtModified);
         return orderPo;
