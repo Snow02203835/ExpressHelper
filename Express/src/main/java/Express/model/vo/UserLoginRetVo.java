3e7af318edcb5679482be8f0c77623855a84539d
Snow02203835 <snow02203835@163.com>
2021-04-28 09:02:20 +0800

    新增 姓名、学号以及学生认证属性并新增相应赋值

--- Express/src/main/java/Express/model/vo/UserLoginRetVo.java
+++ Express/src/main/java/Express/model/vo/UserLoginRetVo.java
@@ -8,19 +8,26 @@ import java.io.Serializable;
 
 /**
  * @author snow create 2021/04/27 20:55
+ *            modified 2021/04/28 08:49
  */
 @Data
 public class UserLoginRetVo implements VoObject, Serializable {
+    private String name;
     private String token;
     private String mobile;
     private String address;
+    private String studentNumber;
+    private Byte studentVerify;
     private Integer credit;
 
     public UserLoginRetVo(String token, User user){
         this.token = token;
+        this.name = user.getName();
         this.credit = user.getCredit();
         this.mobile = user.getDecryptMobile();
         this.address = user.getDecryptAddress();
+        this.studentNumber = user.getStudentNumber();
+        this.studentVerify = user.getStudentVerify();
     }
 
     @Override
